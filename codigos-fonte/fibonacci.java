22d7b25b473f9c209221e2d4cf68eb1f1e4650ba
Cassini0806 <thiagocardoso0806@gmail.com>
2026-09-12 12:40:32 -0300

    added: struct de dados

--- codigos-fonte/fibonacci.java
+++ codigos-fonte/fibonacci.java
@@ -2,10 +2,16 @@
 
 public class fibonacci {
 
+	public class elementos {
+		long a;
+		long b;
+		long ac;
+	}
+
 	public static int fibonacci(int termos) {
-		long a = 0;
-		long b = 1;
-		long ac = 0;
+		a = 0;
+		b = 1;
+		ac = 0;
 		for (int i = 1; i <= termos; i++) {
 			System.out.println(a);
 			ac = a + b;
